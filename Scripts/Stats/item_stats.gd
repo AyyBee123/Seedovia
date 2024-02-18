d8db282fceea845ce43321b92daa4e08b1a71895
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-02-17 20:08:54 -0500

    added rarity stat to item

--- Scripts/Stats/item_stats.gd
+++ Scripts/Stats/item_stats.gd
@@ -1,8 +1,10 @@
 class_name item_stats extends Node
 
 @export var item_name: String
+@export var rarity: int # 1 = common, 5 = legendary (placeholder values just to show that higher = rarer)
 @export var stat_modifiers = {} # might change to an array
 
 func initialize(stats: item_stats):
 	item_name = stats.item_name
+	rarity = stats.rarity
 	stat_modifiers = stats.stat_modifiers
