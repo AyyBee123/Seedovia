a40dbc2582b337fc5656406471c7c95c0f026fe5
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-02-18 16:21:40 -0500

    changed player and fixed inventory crash

--- Scripts/Stats/item_stats.gd
+++ Scripts/Stats/item_stats.gd
@@ -1,10 +1,12 @@
 class_name item_stats extends Node
 
 @export var item_name: String
-@export var rarity: int # 1 = common, 5 = legendary (placeholder values just to show that higher = rarer)
-@export var stat_modifiers = {} # might change to an array
+var category: String # item type (consumable, helmet, gloves, etc)
+var rarity: int # 1 = common, 5 = legendary (placeholder values just to show that higher = rarer)
+var properties: Array[String] # Store properties as string and read them
 
 func initialize(stats: item_stats):
 	item_name = stats.item_name
+	category = stats.category
 	rarity = stats.rarity
-	stat_modifiers = stats.stat_modifiers
+	properties = stats.properties
