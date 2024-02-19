2eba735bab89cd53ece39a311d1533302a1c0fc3
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-02-18 23:08:27 -0500

    added comments in player inventory

--- Scripts/Inventory/player_inventory.gd
+++ Scripts/Inventory/player_inventory.gd
@@ -11,9 +11,11 @@ var equipment = {}#{1: "Basic Helmet", 3: "Basic Body Armour"}
 
 func add_item(item_name):
 	for i in range(NUM_INVENTORY_SLOTS):
-		if not inventory.has(i):
+		if not inventory.has(i): # checks for space in inventory
 			inventory[i] = item_name
 			return
+		else: # if inventory is full
+			pass
 
 func add_item_to_empty_slot(item: item_instance, slot: slot_class):
 	if slot.slot_type == slot_class.slot_types.INVENTORY:
