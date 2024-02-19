493af9e4c8c1436a3d4042971ed18e77dbf5a4cd
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-02-19 12:15:10 -0500

    removed test inventory items

--- Scripts/Inventory/player_inventory.gd
+++ Scripts/Inventory/player_inventory.gd
@@ -9,19 +9,7 @@ var picked_up_item = null
 const NUM_INVENTORY_SLOTS = 12
 
 
-var inventory = {0:load("res://Resources/Items/Consumables/apple.tres"),
-1:load("res://Resources/Items/Consumables/apple.tres"),
-2:load("res://Resources/Items/Consumables/apple.tres"),
-3:load("res://Resources/Items/Consumables/apple.tres"),
-4:load("res://Resources/Items/Consumables/apple.tres"),
-5:load("res://Resources/Items/Consumables/apple.tres"),
-6:load("res://Resources/Items/Consumables/apple.tres"),
-7:load("res://Resources/Items/Consumables/apple.tres"),
-8:load("res://Resources/Items/Consumables/apple.tres"),
-9:load("res://Resources/Items/Consumables/apple.tres"),
-10:load("res://Resources/Items/Consumables/apple.tres"),
-11:load("res://Resources/Items/Consumables/apple.tres"),
-12:load("res://Resources/Items/Consumables/apple.tres"),}
+var inventory = {}
 var equipment = {}#{1: "Basic Helmet", 3: "Basic Body Armour"}
 
 func add_item(item, player, inv):
