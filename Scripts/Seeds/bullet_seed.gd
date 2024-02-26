e8d780a3c0853a9c53d75a78da525122db51b981
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-02-26 05:41:18 -0500

    added buffer for collisions for spawned projectile

--- Scripts/Seeds/bullet_seed.gd
+++ Scripts/Seeds/bullet_seed.gd
@@ -12,6 +12,8 @@ var distance_travelled: float # gets the current range travelled by the bullet
 var position_initialized := false
 
 var initial_weapon := true
+var ignore_first_collision := false # this is to let the projectiles spawn without instantly colliding with an object
+var short_distance_travelled: float # this lets the projectile move a little before enabling collisions again
 
 var slot_index: int
 
@@ -22,6 +24,9 @@ func _physics_process(delta):
 		starting_position = global_position
 		position_initialized = true
 	var collision_detect = move_and_collide(velocity * delta * _player_stats.get_stat("Weapon_Speed"))
+	short_distance_travelled = starting_position.distance_to(self.global_position)
+	if short_distance_travelled >= 1:
+		ignore_first_collision = false
 	distance_travelled = starting_position.distance_to(self.global_position)
 	if distance_travelled >= _player_stats.get_stat("Weapon_Range"):
 		queue_free()
@@ -33,17 +38,19 @@ func _on_bullet_hitbox_area_entered(area):
 	_collide(area)
 	
 func _collide(body):
-	if body.is_in_group("Enemies"):
-		body.get_parent()._enemy_stats.take_damage(_player_stats.get_stat("Weapon_Damage"))
-	for i in range(seed_slots.size()):
-		#if i > slot_index and slot_index < 2:
+	if not ignore_first_collision:
+		if body.is_in_group("Enemies"):
+			body.get_parent()._enemy_stats.take_damage(_player_stats.get_stat("Weapon_Damage"))
+		for i in range(seed_slots.size()):
 			var weapon = PlayerSeeds.load_weapon(slot_index + 1)
 			if weapon != null:
-				shoot_next_weapon(weapon, global_position)
+				shoot_next_weapon(weapon)
 			break
-	queue_free()
+		queue_free()
+	else:
+		ignore_first_collision = false
 	
-func shoot_next_weapon(weapon, location):
+func shoot_next_weapon(weapon):
 	var enemies = get_tree().get_nodes_in_group("Enemies")
 	var nearest_enemy = null
 	var nearest_distance = null
@@ -58,8 +65,12 @@ func shoot_next_weapon(weapon, location):
 	
 	var weapon_instance = weapon.instantiate()
 	weapon_instance.initial_weapon = false
+	weapon_instance.ignore_first_collision = true
 	weapon_instance.slot_index = slot_index + 1
+	call_deferred("initialize_location", weapon_instance, nearest_enemy)
+	
+func initialize_location(weapon_instance, nearest_enemy):
 	get_tree().current_scene.add_child(weapon_instance)
-	weapon_instance.global_position = location
+	weapon_instance.global_position = global_position
 	weapon_instance.velocity = -velocity if nearest_enemy == null else (nearest_enemy.global_position - global_position).normalized()
 	weapon_instance.rotation = weapon_instance.velocity.angle()
