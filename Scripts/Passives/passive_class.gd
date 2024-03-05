2b6184b9843225285613cbb6356b8675d8fce5a4
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-03-04 22:48:58 -0500

    removed rarity from passives and added it to seeds

--- Scripts/Passives/passive_class.gd
+++ Scripts/Passives/passive_class.gd
@@ -3,4 +3,3 @@ class_name passive_class extends Node
 @export var passive_name: String
 @export var sprite: Texture
 @export var description: String
-@export_enum("COMMON", "UNCOMMON", "RARE", "EPIC", "LEGENDARY", "MYSTIC", "N/A:-1") var rarity: int
--- Scripts/Stats/seed_stats.gd
+++ Scripts/Stats/seed_stats.gd
@@ -2,3 +2,4 @@ class_name seed_class extends "res://Scripts/Items/item_class.gd"
 
 @export var scene: PackedScene
 var category := "SEED"
+@export_enum("COMMON", "UNCOMMON", "RARE", "EPIC", "LEGENDARY", "MYSTIC", "N/A:-1") var rarity: int
