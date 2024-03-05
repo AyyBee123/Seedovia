95f2a28b550bdffebf89503e974ddadca1a4b153
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-03-05 14:02:05 -0500

    added explosive trigger passive

--- Scripts/Passives/passive_class.gd
+++ Scripts/Passives/passive_class.gd
@@ -2,4 +2,5 @@ class_name passive_class extends Resource
 
 @export var passive_name: String
 @export var sprite: Texture
-@export var description: String
+@export_multiline var description: String
+@export var properties: PackedScene
