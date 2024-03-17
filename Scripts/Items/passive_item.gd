a86787dad44308e40b75d0b8eeff6a8dd7313a97
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-03-16 23:17:34 -0400

    made passive item automatic pickup

--- Scripts/Items/passive_item.gd
+++ Scripts/Items/passive_item.gd
@@ -1,24 +1,21 @@
 extends Node2D
 
-var player_in_area = false
 var player = null
 
 @onready var resource_preloader := $ResourcePreloader
 
-func _process(delta):
-	if player_in_area:
-		if Input.is_action_just_pressed("pick up"):
-			pick_up()
+func _physics_process(delta):
+	if player != null:
+		position += global_position.direction_to(player.global_position) * delta * 600
 
 func _on_pickable_area_body_entered(body):
 	if body.is_in_group("Players"):
-		player_in_area = true
+		pick_up()
+
+func _on_attract_area_body_entered(body):
+	if body.is_in_group("Players"):
 		player = body
 
-func _on_pickable_area_body_exited(body):
-	if body.is_in_group("Players"):
-		player_in_area = false
-		
 func pick_up():
 	spawn_passive_menu()
 	queue_free()
