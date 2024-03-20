61525aa6835b4bcafe6ce9f4d86e77c17595a06a
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-03-19 22:28:46 -0400

    stored player health

--- Scripts/Player/player_stat_storage.gd
+++ Scripts/Player/player_stat_storage.gd
@@ -2,12 +2,16 @@ extends Node
 
 @onready var player = get_tree().get_nodes_in_group("Players")[0] # keeps returning null, so I added it to the setget
 var stats: Dictionary
+var current_health: int
 
 func get_stats():
 	player = get_tree().get_nodes_in_group("Players")[0]
 	stats = player._player_stats.stats
+	current_health = player._player_stats.health
 	return stats
 
 func set_stats():
 	player = get_tree().get_nodes_in_group("Players")[0]
 	player._player_stats.stats = stats
+	player._player_stats.health = current_health
+	
--- Scripts/Save Data/player_data.gd
+++ Scripts/Save Data/player_data.gd
@@ -6,6 +6,7 @@ class_name player_data extends Resource
 @export var seeds: Dictionary = PlayerInventory.seeds
 @export var passives: Array
 @export var stats: Dictionary
+@export var current_health: int
 
 func get_inventory():
 	inventory = PlayerInventory.inventory
@@ -26,7 +27,9 @@ func set_passives():
 
 func get_stats():
 	stats = PlayerStatStorage.get_stats()
+	current_health = PlayerStatStorage.current_health
 
 func set_stats():
 	PlayerStatStorage.stats = stats
+	PlayerStatStorage.current_health = current_health
 	PlayerStatStorage.set_stats()
