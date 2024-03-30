b0f933a6a043ca54e76f27291d819656d26ed28a
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-03-30 14:28:00 -0400

    added doors to target

--- Scripts/Global/targets.gd
+++ Scripts/Global/targets.gd
@@ -3,8 +3,10 @@ extends Node
 var player
 var enemies
 var items
+var doors
 
 func _ready():
 	player = get_tree().get_first_node_in_group("Players")
 	enemies = get_tree().get_nodes_in_group("Enemy")
 	items = get_tree().get_nodes_in_group("Item")
+	doors = get_tree().get_nodes_in_group("Door")
