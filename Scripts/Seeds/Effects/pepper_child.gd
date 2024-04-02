df24121a9a504b518401696ff5748ebee8bba1e2
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-04-02 15:54:16 -0400

    minor changes

--- Scripts/Seeds/Effects/pepper_child.gd
+++ Scripts/Seeds/Effects/pepper_child.gd
@@ -22,14 +22,16 @@ func initialize_position():
 		position_initialized = true
 
 func update_position(delta):
-	var current_velocity: Vector2 = direction * _player_stats.get_stat("Weapon_Speed") * speed_multiplier * projectile_speed_timer.time_left
+	var current_velocity: Vector2 = direction * _player_stats.get_stat("Weapon_Speed") *\
+	speed_multiplier * projectile_speed_timer.time_left
 	position += current_velocity * delta
 	look_at(global_position + current_velocity)
 
 func _on_hitbox_area_entered(area):
 	has_collided.emit(area)
 	if area.is_in_group("Enemies"):
-			area.get_parent()._enemy_stats.take_damage(_player_stats.get_stat("Weapon_Damage") * damage_multiplier / 2)
+			area.get_parent()._enemy_stats.take_damage(_player_stats.get_stat("Weapon_Damage") *\
+			damage_multiplier / 2)
 	explode()
 
 func _on_hitbox_body_entered(body):
@@ -47,7 +49,8 @@ func explode():
 	call_deferred("create_child", explosion)
 	attempted_fire.emit()
 	for i in range(seed_slots.size()):
-		var weapon = null if PlayerSeeds.seeds.size() <= 1 + slot_index or slot_index >= 2 else PlayerSeeds.seeds[slot_index + 1]
+		var weapon = null if PlayerSeeds.seeds.size() <= 1 + slot_index or\
+		slot_index >= 2 else PlayerSeeds.seeds[slot_index + 1]
 		if weapon != null:
 			shoot_next_weapon(weapon)
 		break
