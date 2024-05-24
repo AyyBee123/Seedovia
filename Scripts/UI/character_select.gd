c276a791ebc3435f2bdf9ea234c4d0904b26e6b7
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-05-24 13:37:20 -0400

    small timer in character select to prevent shooting immediately

--- Scripts/UI/character_select.gd
+++ Scripts/UI/character_select.gd
@@ -10,4 +10,5 @@ func _ready():
 func select_gui_input(event: InputEvent, char_select: character):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT && event.pressed:
+			await get_tree().create_timer(0.05).timeout
 			char_select.select_character()
