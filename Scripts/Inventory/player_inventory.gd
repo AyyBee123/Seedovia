771ce9e1431bb7de67bf0281ae7ff1f7fcbad9af
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-07-21 22:35:08 -0400

    added sunder (inventory sprite placeholder)

--- Scripts/Inventory/player_inventory.gd
+++ Scripts/Inventory/player_inventory.gd
@@ -9,7 +9,7 @@ const NUM_INVENTORY_SLOTS = 12
 const NUM_TALISMAN_SLOTS = 4
 const NUM_SEED_SLOTS = 3
 
-var inventory = {11:preload("res://Resources/Items/Seeds/bananarang.tres")}
+var inventory = {11:preload("res://Resources/Items/Seeds/sunder.tres")}
 var talismans = {}
 var seeds = {}
 
---
+++ Scripts/Seeds/sunder.gd
@@ -0,0 +1,34 @@
+extends "res://Scripts/Seeds/seed_template.gd"
+
+@onready var resource_preloader = $ResourcePreloader
+
+func travelled_distance():
+	distance_travelled = starting_position.distance_to(global_position)
+	if distance_travelled >= 1:
+		total_distance += 1
+		starting_position = global_position
+	if total_distance >= _player_stats.get_stat("Weapon_Range") * range_multiplier:
+		call_deferred("free")
+	if total_distance % 10 == 0:
+		explode()
+		weapon_direction = Vector2.RIGHT.rotated(randf_range(0, 2 * PI))
+		if total_distance % 20 == 0 and total_distance != 0:
+			shoot_next_weapon()
+
+func _collide(body):
+	if ignore_first_collision:
+		ignore_first_collision = false
+		return
+	has_collided.emit(body) # for on-hit effects (ex: burning an enemy on hit)
+	call_deferred("free")
+
+func explode():
+	var explosion = resource_preloader.get_resource("Explosion").instantiate()
+	explosion.damage = _player_stats.get_stat("Weapon_Damage") * damage_multiplier
+	explosion.size = _player_stats.get_stat("Weapon_Blast_Radius") * blast_radius_multiplier
+	explosion.get_node("AnimatedSprite2D").self_modulate = Color.SADDLE_BROWN
+	call_deferred("create_child", explosion)
+
+func create_child(child):
+	get_tree().current_scene.add_child(child)
+	child.global_position = global_position
