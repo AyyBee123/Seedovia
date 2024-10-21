4983fef4b4e0361bb1493c365df9ea642455cd44
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-10-21 15:30:01 +0400

    added dancing mushroom enemy

---
+++ Scripts/Enemies/Dancing Mushroom/dancing_mushroom.gd
@@ -0,0 +1,18 @@
+extends "res://Scripts/Enemies/enemy.gd"
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var resource_preloader = $ResourcePreloader
+var bullet = preload("res://Scenes/Enemies/Weapons/Mushroom Bullet.tscn")
+
+func _on_animated_sprite_2d_animation_looped():
+	shoot_bullet()
+
+func shoot_bullet():
+	var bullet_instance = bullet.instantiate()
+	bullet_instance.damage = _enemy_stats.weapon_damage
+	bullet_instance.range = _enemy_stats.weapon_range
+	bullet_instance.speed = _enemy_stats.weapon_speed
+	while bullet_instance.direction == Vector2.ZERO:
+		bullet_instance.direction = Vector2(randf_range(-1, 1), randf_range(-1, 1)).normalized()
+	get_tree().current_scene.add_child(bullet_instance)
+	bullet_instance.global_position = global_position + bullet_instance.direction * 10
---
+++ Scripts/Enemies/Weapons/mushroom_bullet.gd
@@ -0,0 +1,37 @@
+extends "res://Scripts/Enemies/Weapons/bullet.gd"
+
+@onready var deceleration = $"Deceleration"
+@onready var lifetime = $Lifetime
+
+var max_range_reached := false
+var decelerating := false
+
+func _ready():
+	super._ready()
+	rotation = randf_range(0, 2 * PI)
+
+func update_position(delta):
+	var current_velocity: Vector2
+	if max_range_reached:
+		current_velocity = direction * max(speed * deceleration.time_left / deceleration.wait_time, 10)
+	else:
+		current_velocity = direction * speed
+	position += current_velocity * delta
+
+func travelled_distance():
+	distance_travelled = starting_position.distance_to(self.global_position)
+	if distance_travelled >= range:
+		max_range_reached = true
+		if not decelerating:
+			deceleration.start()
+			decelerating = true
+
+func _collide(body):
+	if body.is_in_group("Players"):
+		player._player_stats.take_damage(self)
+	# if the bullet hits anything else (i.e. a wall)
+	else:
+		queue_free()
+
+func _on_lifetime_timeout():
+	queue_free()
