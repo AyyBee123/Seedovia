6b7030eadaa2f80c6a48aea7f32a7870384cc13c
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-11-09 22:20:51 +0400

    removed game save when holding an item

--- Scripts/Inventory/inventory_slot.gd
+++ Scripts/Inventory/inventory_slot.gd
@@ -34,8 +34,7 @@ func pick_from_slot():
 	if slot_type == slot_types.SEED:
 		PlayerSeeds.load_weapons()
 		player.update_timers()
-	Global.save_data()
-	
+
 func put_into_slot(new_item):
 	item = new_item
 	item.position = Vector2(size.x / 2, size.y / 2)
@@ -51,7 +50,6 @@ func put_into_slot(new_item):
 	if slot_type == slot_types.SEED:
 			PlayerSeeds.load_weapons()
 			player.update_timers()
-	Global.save_data()
 
 func initialize_item(slot_item):
 	item_instance = load("res://Scenes/Items/item.tscn")
