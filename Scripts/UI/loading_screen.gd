4c9483051dd73e8348c8352133a0e03a4cc94fd2
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-11-11 12:11:09 +0400

    added loading screen (doesn't work though)

---
+++ Scripts/UI/loading_screen.gd
@@ -0,0 +1,37 @@
+extends Control
+
+var scene_to_load_path
+var loading = false
+var progress = []
+
+func _ready():
+	ResourceLoader.load_threaded_request(LevelList.loaded_current_room)
+
+func load_scene(path):
+	if ResourceLoader.has_cached(path):
+		ResourceLoader.load_threaded_get(path)
+	else:
+		ResourceLoader.load_threaded_request(path)
+	
+	loading = true
+	scene_to_load_path = path
+
+func load_save():
+	Global.load_room()
+	Pool.continue_run()
+
+func _process(delta):
+	if not loading:
+		return
+	
+	var status = ResourceLoader.load_threaded_get_status(scene_to_load_path, progress)
+	
+	if status == ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+		$ProgressBar.value = progress[0] * 100
+		$ProgressNumber.text = str(progress[0] * 100) + "%"
+	elif status == ResourceLoader.THREAD_LOAD_LOADED:
+		get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(scene_to_load_path))
+		queue_free()
+		loading = false
+	else:
+		print("Error loading...")
--- Scripts/UI/main_menu.gd
+++ Scripts/UI/main_menu.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var continue_button = $"Continue Button"
 @onready var character_select_scene = preload("res://Scenes/UI/Character Select.tscn")
+@onready var loading_screen_scene = preload("res://Scenes/UI/Loading Screen.tscn")
+var loading_screen_scene_instance
 
 func _ready():
 	if ResourceLoader.exists(Global.SAVE_PATH):
@@ -13,6 +15,7 @@ func _on_play_button_pressed():
 	get_tree().change_scene_to_packed(character_select_scene)
 
 func _on_continue_button_pressed():
-	Global.load_room()
-	Pool.continue_run()
-	get_tree().change_scene_to_packed(load(LevelList.loaded_current_room))
+	loading_screen_scene_instance = loading_screen_scene.instantiate()
+	get_tree().root.add_child.call_deferred(loading_screen_scene_instance)
+	loading_screen_scene_instance.load_save()
+	loading_screen_scene_instance.load_scene(LevelList.loaded_current_room)
