d65e2a0009afed6305780d0a65e45301b343b78b
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-11-14 13:43:31 +0400

    Update walnut.gd

--- Scripts/Seeds/walnut.gd
+++ Scripts/Seeds/walnut.gd
@@ -10,8 +10,11 @@ func _ready():
 	orb_fire_rate.start(1.0/(player._player_stats.get_stat("Fire_Rate") * orb_fire_rate_multiplier))
 
 func _physics_process(delta):
-	rotation_degrees += 45.0 * (player._player_stats.get_stat("Fire_Rate") * orb_fire_rate_multiplier \
-			* get_next_weapon().instantiate().fire_rate_multiplier * 2) * delta
+	if get_next_weapon():
+		rotation_degrees += 45.0 * player._player_stats.get_stat("Fire_Rate") * orb_fire_rate_multiplier \
+				* get_next_weapon().instantiate().fire_rate_multiplier * 2 * delta
+	else:
+		rotation_degrees += 45.0 * player._player_stats.get_stat("Fire_Rate") * orb_fire_rate_multiplier * delta
 	super._physics_process(delta)
 	if orb_fire_rate.is_stopped():
 		shoot_next_weapon()
