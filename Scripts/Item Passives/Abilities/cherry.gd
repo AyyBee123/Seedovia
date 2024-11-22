b98e3848e18af67002e9ef4f77bb0d5c5bdcc887
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-11-22 13:53:59 +0400

    added cherry talisman

---
+++ Scripts/Item Passives/Abilities/cherry.gd
@@ -0,0 +1,21 @@
+extends "res://Scripts/Passives/Classes/passive_chance.gd"
+
+func _ready():
+	super._ready()
+	chance = 0.2
+	player.weapon_fired.connect(chance_to_trigger)
+
+func trigger(weapon = null):
+	if weapon == null:
+		return
+	await get_tree().create_timer(1.0/(player._player_stats.get_stat("Fire_Rate") \
+			* weapon.fire_rate_multiplier * 2)).timeout
+	var weapon_instance = PlayerSeeds.get_weapon(0).instantiate()
+	var location = player.hand.global_position
+	weapon_instance.initial_weapon = true
+	weapon_instance.slot_index = 0
+	weapon_instance.seed_slot_number = PlayerSeeds.seed_indices[0]
+	weapon_instance.desired_direction = location.direction_to(player.weapon_direction_marker.global_position)
+	get_tree().current_scene.add_child(weapon_instance)
+	weapon_instance.global_position = location
+	player.weapon_fired.emit(weapon_instance)
