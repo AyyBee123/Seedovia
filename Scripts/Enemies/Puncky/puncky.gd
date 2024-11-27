b3f52b5582ad3f729d0062bba483e63648f92c4c
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-11-27 15:54:44 +0400

    fixed Puncky

--- Scripts/Enemies/Puncky/puncky.gd
+++ Scripts/Enemies/Puncky/puncky.gd
@@ -11,10 +11,14 @@ var puck_direction: Vector2
 
 func find_puck():
 	var pucks = get_tree().get_nodes_in_group("Puck")
-	return pucks.pick_random()
+	if pucks:
+		return pucks.pick_random()
+	else:
+		return null
 
 func idle():
 	_puck_launched = false
+	puck = find_puck()
 
 func ready_up():
 	if puck:
@@ -22,6 +26,9 @@ func ready_up():
 		velocity = velocity.lerp(direction.normalized() * _enemy_stats.speed, _enemy_stats.acceleration)
 		if ($Shadow.global_position - puck.global_position).length() <= 10:
 			_is_ready = true
+	else:
+		var direction = global_position # leaves the room
+		velocity = velocity.lerp(direction.normalized() * _enemy_stats.speed, _enemy_stats.acceleration)
 
 func punch():
 	velocity = Vector2.ZERO
@@ -33,10 +40,12 @@ func punch():
 func _on_animated_sprite_2d_animation_finished():
 	if animated_sprite_2d.animation == "Ready Beginning":
 		animated_sprite_2d.play("Ready")
-		puck = find_puck()
 		if puck == null: # if there are no pucks in the level
 			_no_pucks = true
 	if animated_sprite_2d.animation == "Punch":
 		_punched = true
 		puck = null
 		puck_direction = Vector2.ZERO
+
+func _on_visible_on_screen_enabler_2d_screen_exited():
+	queue_free()
