5c6bafc81306da5a53d579ebaef54da6233cedf0
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-12-02 14:29:32 +0400

    Update fertilizer.gd

--- Scripts/Seeds/fertilizer.gd
+++ Scripts/Seeds/fertilizer.gd
@@ -10,6 +10,8 @@ var time_to_live = 5
 var is_in_area := false
 var enemy = null
 var hit_wall := false
+var enemies_in_area: Array
+var tick_timers: Array
 
 func _ready():
 	super._ready()
@@ -23,11 +25,14 @@ func _ready():
 
 func _physics_process(delta):
 	super._physics_process(delta)
-	if is_in_area:
-		if tick_rate.is_stopped():
-			enemy._enemy_stats.take_damage(player._player_stats.get_stat("Weapon_Damage") * damage_multiplier)
-			has_collided.emit(enemy.get_node("Enemy Hitbox"))
-			tick_rate.start()
+	# damage multiple enemies at a time
+	for i in enemies_in_area.size():
+		if tick_timers[i].is_stopped():
+			if is_instance_valid(enemies_in_area[i]):
+				enemies_in_area[i]._enemy_stats.take_damage(player._player_stats.get_stat("Weapon_Damage") \
+				* damage_multiplier)
+				has_collided.emit(enemies_in_area[i].get_node("Enemy Hitbox"))
+				tick_timers[i].start(tick_rate.wait_time / _player_stats.get_stat("Fire_Rate") * 20)
 
 func travelled_distance():
 	pass
@@ -37,7 +42,8 @@ func _collide(body):
 
 func update_position(delta):
 	if not hit_wall:
-		current_velocity = direction * player._player_stats.get_stat("Weapon_Speed") * speed_multiplier * deceleration.time_left
+		current_velocity = direction * player._player_stats.get_stat("Weapon_Speed") * speed_multiplier \
+				* deceleration.time_left
 		position += current_velocity * delta
 
 func _on_deceleration_timeout():
@@ -56,12 +62,20 @@ func shoot_next_weapon():
 
 func _on_hurtbox_area_entered(area):
 	if area.is_in_group("Enemies"):
-		enemy = area.get_parent()
-		is_in_area = true
+		if is_instance_valid(area):
+			enemies_in_area.append(area.get_parent())
+			var timer = Timer.new()
+			add_child(timer)
+			timer.wait_time = tick_rate.wait_time / _player_stats.get_stat("Fire_Rate") * 20
+			timer.one_shot = true
+			tick_timers.append(timer)
 
 func _on_hurtbox_area_exited(area):
 	if area.is_in_group("Enemies"):
-		is_in_area = false
+		if is_instance_valid(area):
+			var index = enemies_in_area.find(area.get_parent())
+			enemies_in_area.remove_at(index)
+			tick_timers.remove_at(index)
 
 func _on_hitbox_body_entered(body):
 	hit_wall = true
