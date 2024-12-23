1d74881bb9ba169d6d82a0a897fa0c3ac967b61a
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2024-12-23 16:07:32 +0400

    added screenshake

--- Scripts/Enemies/Jellofish/state_machine.gd
+++ Scripts/Enemies/Jellofish/state_machine.gd
@@ -41,6 +41,7 @@ func _enter_state(new_state, old_state):
 		states.launch:
 			parent.launch_SFX.play()
 		states.stun:
+			Targets.get_camera().add_trauma(0.2)
 			parent.impact_SFX.play()
 			parent.animated_sprite_2d.play("Stun")
 
