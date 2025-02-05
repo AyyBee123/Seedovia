0d06957e5d353f20cb168860f27cd423fec7cf45
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2025-02-05 15:55:23 +0400

    added Hot Pot enemy

---
+++ Scripts/Enemies/Hot Pot/hot_pot.gd
@@ -0,0 +1,25 @@
+extends "res://Scripts/Enemies/enemy.gd"
+
+@onready var pointer = $Pointer
+@onready var marker_2d = $Pointer/Marker2D
+
+var direction: Vector2
+var rotation_speed: float
+
+func _ready():
+	super._ready()
+	randomize()
+	rotation_speed = randf_range(1, 8)
+	
+	# look at the player at the start to avoid turning when entering a new room
+	if player:
+		pointer.rotation = global_position.direction_to(player.global_position).angle()
+
+func _physics_process(delta):
+	super._physics_process(delta)
+	if player:
+		direction = global_position.direction_to(player.global_position)
+		pointer.rotation = lerp_angle(pointer.rotation, direction.angle(), rotation_speed * delta)
+		velocity = global_position.direction_to(marker_2d.global_position) * _enemy_stats.speed
+	
+	move_and_slide()
