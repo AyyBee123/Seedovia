fbbc08a5510a4c35acef323d748e988aae9062aa
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2025-05-23 11:03:44 +0400

    Update apple.gd

--- Scripts/Consumables/apple.gd
+++ Scripts/Consumables/apple.gd
@@ -1,5 +1,10 @@
 class_name apple_consumable extends consumable_item_class
 
+var used: bool
+
 func on_use() -> void:
+	if used:
+		return
 	var player = Targets.get_player()
 	player._player_stats.heal(1) # heal player by 1 hp
+	used = true
