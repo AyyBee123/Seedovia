7c215fe60c671e0b3a155688aef28a8eca9308d3
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2025-06-03 22:30:33 +0400

    Update pizza_components.gd

--- Scripts/Seeds/pizza_components.gd
+++ Scripts/Seeds/pizza_components.gd
@@ -55,17 +55,3 @@ func initialize_location(weapon):
 	weapon.add_child(get_node("Passives").duplicate())
 	get_tree().current_scene.add_child(weapon)
 	weapon.global_position = global_position
-
-func get_all_file_paths(path: String) -> Array[String]:
-	var file_paths: Array[String] = []
-	var dir = DirAccess.open(path)
-	dir.list_dir_begin()
-	var file_name = dir.get_next()
-	while file_name != "":
-		var file_path = path + "/" + file_name
-		if dir.current_is_dir():
-			file_paths += get_all_file_paths(file_path)
-		else:
-			file_paths.append(file_path)
-		file_name = dir.get_next()
-	return file_paths
