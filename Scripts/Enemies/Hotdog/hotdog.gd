f769ef15d87954075e9f34cbfb1914c14b683388
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2025-06-09 12:25:56 +0400

    Update hotdog.gd

--- Scripts/Enemies/Hotdog/hotdog.gd
+++ Scripts/Enemies/Hotdog/hotdog.gd
@@ -17,12 +17,14 @@ var tween
 
 func _physics_process(delta):
 	super._physics_process(delta)
+	# set sizes and positions in real-time to match the hotdog length
 	var length = 27 + hotdog_sprite.get_region_rect().size.x / 2 * (hotdog_sprite.scale.x - 1)
 	left.position.x = -length
 	right.position.x = length
 	hotdog_collision.shape.size.x = 12 + hotdog_sprite.scale.x * 46
 
 func idle():
+	# slow the hotdog rotation down to a stop
 	animated_sprite_2d.rotation += get_physics_process_delta_time() \
 			* deceleration.time_left / deceleration.wait_time * SPIN_SPEED
 	hotdog_sprite.rotation = animated_sprite_2d.rotation
@@ -32,6 +34,7 @@ func idle():
 	move_and_slide()
 
 func spin():
+	# rotate the hotdog
 	animated_sprite_2d.rotation += get_physics_process_delta_time() \
 			* SPIN_SPEED * (1 - (acceleration.time_left / acceleration.wait_time))
 	hotdog_sprite.rotation = animated_sprite_2d.rotation
@@ -43,11 +46,13 @@ func spin():
 	move_and_slide()
 
 func stretch():
+	# stretch the hotdog
 	stretch_SFX.play()
 	tween = get_tree().create_tween()
 	tween.tween_property(hotdog_sprite, "scale:x", MAX_LENGTH, 1)
 
 func shrink():
+	# shrink the hotdog
 	tween = get_tree().create_tween()
 	tween.tween_property(hotdog_sprite, "scale:x", 1, 0.5)
 
