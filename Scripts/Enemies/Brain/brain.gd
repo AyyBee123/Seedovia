4d1d6966933fdcc8a68015bbe16c3210e3cf00d7
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2025-06-10 19:10:28 +0400

    brain done

--- Scripts/Enemies/Brain/brain.gd
+++ Scripts/Enemies/Brain/brain.gd
@@ -1,7 +1,7 @@
 extends "res://Scripts/Enemies/enemy.gd"
 
-@onready var downward_direction_point = $"Downward Direction Point"
-@onready var forward_direction_point = $"Forward Direction Point"
+@onready var downward_direction_point = %"Downward Direction Point"
+@onready var forward_direction_point = %"Forward Direction Point"
 @onready var fire_time = $"Fire Time"
 
 var bullet = preload("res://Scenes/Enemies/Weapons/Bullet.tscn")
@@ -9,38 +9,44 @@ var bullet = preload("res://Scenes/Enemies/Weapons/Bullet.tscn")
 var downward_direction
 var forward_direction
 var initial_downward_velocity
-var falling = true
 var direction = 1
+var rot
 
 func _ready():
+	randomize()
 	super._ready()
-	downward_direction = global_position.direction_to(downward_direction_point.global_position).normalized()
+	downward_direction = global_position.direction_to(downward_direction_point.global_position)
+	fire_time.wait_time = randf_range(1, 1.2)
+	
+	rot = rotation
+	rotation = 0
+	$AnimatedSprite2D.rotation = rot
+	$"Enemy Hitbox/CollisionPolygon2D".rotation = rot
+	$CollisionPolygon2D.rotation = rot
+	$"Wall Detect".rotation = rot
+	$Node2D.rotation = rot
 
 func _physics_process(delta):
 	super._physics_process(delta)
-	$"Rotation Point".look_at(player.global_position)
-
-func move_down():
-	velocity = velocity.lerp(downward_direction.normalized() * _enemy_stats.speed, _enemy_stats.acceleration)
+	if player:
+		$"Rotation Point".look_at(player.global_position)
+	velocity = velocity.lerp(downward_direction * _enemy_stats.speed, _enemy_stats.acceleration)
+	move_forward()
+	
+	move_and_slide()
 
 func move_forward():
-	forward_direction = global_position.direction_to(forward_direction_point.global_position).normalized()
+	forward_direction = global_position.direction_to(forward_direction_point.global_position)
 	velocity = velocity.lerp(forward_direction * _enemy_stats.speed * direction, _enemy_stats.acceleration)
 	shoot()
-
-func _on_floor_detect_body_entered(body):
-	falling = false
 
 func _on_wall_detect_body_entered(body):
 	direction *= -1
 
 func shoot():
-	var can_fire = false
 	if fire_time.is_stopped():
-		can_fire = true
-		fire_time.start()
-	if can_fire:
 		shoot_bullet()
+		fire_time.start(1)
 
 func shoot_bullet():
 	var bullet_instance = bullet.instantiate()
