cb98eeab1af9f5927b03117bba81d82c6af5eb4e
Abdalla Joumaa <49286614+AyyBee123@users.noreply.github.com>
2025-07-20 21:03:17 +0400

    fixed pot talismans not working

--- Scripts/Item Passives/Abilities/empty_pot_effect.gd
+++ Scripts/Item Passives/Abilities/empty_pot_effect.gd
@@ -1,7 +1,11 @@
 extends "res://Scripts/Passives/Classes/passive_slot_specific.gd"
 
+var source
+
 func _ready():
 	slot_number = 0
+	source = get_parent().get_parent()
+	source.weapon_fired.connect(get_slot_number)
 	super._ready()
 
 func trigger(weapon):
